6524699caeaa327630a27391d11f6cf0978e7139
munnatm <135148810+munnatm@users.noreply.github.com>
2025-08-21 12:06:01 +0600

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ function showMessage(response) {
       "I am so lucky to have found you. You are the love of my life, and I want to spend every moment with you. Will you be mine forever?"
     );
     image.src = "2.jpg"; // Replace with the path to the "Yes" image
+    // center image on page
+    image.style.position = "absolute";
+    image.style.top = "50%";
+    image.style.left = "50%";
+    image.style.transform = "translate(-50%, -50%)";
+
+    // add hover effect in JS
+    image.onmouseover = () => {
+      image.style.transform = "translate(-50%, -50%) scale(1.1)";
+    };
+    image.onmouseout = () => {
+      image.style.transform = "translate(-50%, -50%) scale(1)";
+    };
   } else if (response === "no") {
     alert(
       "I understand. My love for you will always remain, and I respect your decision."
